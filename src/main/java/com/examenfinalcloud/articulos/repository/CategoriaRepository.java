907d2b6aff5e1cc171b154c0dcbee0b716d7dfe2
Josue0625 <jcampos@ufpso.edu.co>
2022-12-15 18:18:31 -0500

    Creacion del repositorio de categoria

--- src/main/java/com/examenfinalcloud/articulos/repository/CategoriaRepository.java
+++ src/main/java/com/examenfinalcloud/articulos/repository/CategoriaRepository.java
@@ -1,2 +1,9 @@
-package com.examenfinalcloud.articulos.repository;public interface CategoriaRepository {
+package com.examenfinalcloud.articulos.repository;
+//Se importaron las librerias a usar dentro del modelo de categoria
+import com.examenfinalcloud.articulos.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+    Optional<Categoria> findById(Long id);//Se crea un metodo para buscar la categoria por ID
 }
