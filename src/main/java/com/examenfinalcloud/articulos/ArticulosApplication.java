316bbedc502d97931fe7abb8344e615ac711b2fc
YEISON ASCANIO <ydascanioa@ufpso.edu.co>
2022-12-15 17:16:51 -0500

    Actualizamos las dependencias y etamos creando el modelos de users

--- src/main/java/com/examenfinalcloud/articulos/ArticulosApplication.java
+++ src/main/java/com/examenfinalcloud/articulos/ArticulosApplication.java
@@ -1,11 +1,7 @@
-package com.primerparcialpn.articulos;
+package com.examenfinalcloud.articulos;
 
-import com.primerparcialpn.articulos.models.Articulo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -15,9 +11,5 @@ public class ArticulosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArticulosApplication.class, args);
 	}
-	//List<Articulo> findAllByCodigo(String codigo);
-	@Bean
-	public PasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
+
 }
