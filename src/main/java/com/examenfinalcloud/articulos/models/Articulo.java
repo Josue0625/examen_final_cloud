75511e20520c31cd1fef66ba81789b32f47333b9
HaroldMtz21 <hfmartinezc@ufpso.edu.co>
2022-12-15 20:03:31 -0500

    Se agrego el modelo articulo

---
+++ src/main/java/com/examenfinalcloud/articulos/models/Articulo.java
@@ -0,0 +1,36 @@
+package com.examenfinalcloud.articulos.models;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "articulos")
+
+public class Articulo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50, unique = true)
+    private String codigo;
+    @Column(length = 100, nullable = false)
+    private String nombre;
+    @Column(length = 500)
+    private String descripcion;
+    @Column(nullable = false)
+    private Date fecha;
+    @Column(length = 50, nullable = false)
+    private int stock;
+    @Column(length = 100, nullable = false)
+    private double venta;
+    @Column(length = 100, nullable = false)
+    private int compra;
+    @ManyToOne
+    private  Categoria categoria;
+    @ManyToOne
+    private User user;
+
+}
